4b8eb8d86e976449fb930635601538384ccab987
jesbadu <barreras.jesus@uabc.edu.mx>
2025-02-25 11:33:45 -0800

    segunda version

---
+++ scripspt1.js
@@ -0,0 +1,29 @@
+// keyword variableName = value;
+// var, let, const
+
+// String
+var firstName = "Jesus";
+var lastName = "Barreras";
+
+firstName = "Francisco";
+console.log(firstName);
+
+// numerical
+let myAge = 99;
+let myHeight = 100;
+
+console.log(myAge, myHeight);
+
+// boolean (true/false)
+let isStudent = true;
+
+// math operations + - / *
+let num1 = 10;
+let num2 = 12;
+
+console.log("El resultado es = " + (num1 + num2));
+console.log("El resultado es = " + (num1 - num2));
+console.log("El resultado es = " + (num1 * num2));
+console.log("El resultado es = " + (num1 / num2).toFixed(3));
+
+alert ("esto es un mensaje de alerta");
